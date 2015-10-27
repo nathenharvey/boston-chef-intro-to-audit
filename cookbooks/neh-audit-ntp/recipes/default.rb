cc5cf128778f0f6a427f6cb5cc1e75616344f612
John Doe <jdoe@example>
2015-10-27 22:06:07 +0000

    Add an control_group for ntp

--- cookbooks/neh-audit-ntp/recipes/default.rb
+++ cookbooks/neh-audit-ntp/recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+control_group 'neh - ntp' do
+  control 'ntp is running and enabled' do
+    describe service('ntp') do
+      it { should be_running }
+      it { should be_enabled }
+    end
+  end
+end
+
